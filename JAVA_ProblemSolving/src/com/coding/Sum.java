1aa123117b198b04324060ff68f15636a7398441
송다혜 <dada3254@daum.net>
2021-01-01 20:03:35 +0900

    210101 SWEA1209.Sum (UP)

--- JAVA_ProblemSolving/src/com/coding/Sum.java
+++ JAVA_ProblemSolving/src/com/coding/Sum.java
@@ -1,7 +1,9 @@
 package com.coding;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 //21.01.01
 //1209. [S/W 문제해결 기본] 2일차 - Sum D3
@@ -10,7 +12,7 @@ public class Sum {
         Scanner sc = new Scanner(System.in);
         for (int t = 0; t < 10; t++) {
             // 입력
-            int result = 0;
+
             int tc = sc.nextInt();
             int[][] arr = new int[100][100];
             for (int y = 0; y < 100; y++) {
@@ -18,36 +20,20 @@ public class Sum {
                     arr[x][y] = sc.nextInt();
                 }
             }
-            int garoSum;
-            int seroSum=0;
-            int ltor=0;
-            int rtol=0;
-            // 문제풀이
+
+            int max = -1;
             for (int y = 0; y < 100; y++) {
-                //대각선
-                ltor += arr[y][y];
-                rtol += arr[y][99-y];
-                //가로 행
-                garoSum = Arrays.stream(arr[y]).sum();
-                if (result < garoSum) {
-                    result = garoSum;
-                }
-                //세로 열
-                for (int x = 0; x < 100; x++) {
-                    seroSum += arr[x][y];
-                }
-                if (result < seroSum) {
-                    result = seroSum;
-                }
-                seroSum =0;
+                int finalY = y;
+                int sum1 = IntStream.range(0, 100).map(it -> arr[finalY][it]).sum();
+                int sum2 = IntStream.range(0, 100).map(it -> arr[it][finalY]).sum();
+
+                max = Math.max(Math.max(sum1, sum2), max);
             }
-            if (result < ltor) {
-                result = ltor;
-            }
-            if (result < rtol) {
-                result = rtol;
-            }
-            System.out.println("#"+tc+" "+result);
+            int cross1 = IntStream.range(0, 100).map(it -> arr[it][it]).sum();
+            int cross2 = IntStream.range(0, 100).map(it -> arr[99-it][99-it]).sum();
+
+            max = Math.max(max, Math.max(cross1, cross2));
+            System.out.println("#"+tc+" "+max);
         }
 
     }
