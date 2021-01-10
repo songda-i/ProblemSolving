eb2f47bdc77fb80ef4bacb999cfb94e06c54e938
송다혜 <dada3254@daum.net>
2021-01-10 16:48:28 +0900

    210110 SWEA1213.String 정규식

--- JAVA_ProblemSolving/src/com/coding/Longstring.java
+++ JAVA_ProblemSolving/src/com/coding/Longstring.java
@@ -1,6 +1,8 @@
 package com.coding;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //21.01.10
 //1213. [S/W 문제해결 기본] 3일차 - String D3
@@ -12,34 +14,13 @@ public class Longstring {
             tc = sc.nextInt();
             int cnt = 0;
             String shorts = sc.next(); //10자이내
-            String[] shortArr = shorts.split("");
-
-            //System.out.println( java.util.Arrays.toString(shortArr) );
             String longs = sc.next(); //1000자이내
-            String[] longArr = longs.split("");
-            //System.out.println( java.util.Arrays.toString(longArr) );
-            //System.out.println(longArr.length);
-            // 긴 문자열 속에 작은 문자열이 일치하는 횟수 ++1
-            for(int j=0;j<longArr.length-shortArr.length+1;j++){
-                /*  첫 글자가 맞으면
-                    작은 문자열 갯수대로 그 뒤 맞춰보기
-                * */
-                if(longArr[j].equals(shortArr[0])){
-                    boolean go = true;
-                    for(int k =1;k<shortArr.length;k++){
-                        if(!longArr[j+k].equals(shortArr[k])){
-                            go = false;
-                            break;
-                        }
-                    }
-                    if(go){
-                        cnt++;
-                        //System.out.println(j);
-                    }
-                }
+            // 정규식
+            Pattern regex = Pattern.compile(shorts);
+            Matcher matcher = regex.matcher(longs);
+            while(matcher.find()){
+                cnt++;
             }
-//            System.out.println(arr[0]);
-
             System.out.println("#"+tc+" "+cnt);
 
         }
